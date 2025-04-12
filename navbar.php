5e975c699f79828f831d32ee5a40364687ac1b4f
Bogdan981011 <122132248+Bogdan981011@users.noreply.github.com>
2025-04-12 04:17:24 +0200

    Update paths to pics

--- navbar.php
+++ navbar.php
@@ -2,32 +2,32 @@
     <div class="menu-bar">
     <div class="menu-item">
     <a href="https://map-system-production.up.railway.app/questionnaire.php">
-        <img src="/images/images_ced/icone1.png" alt="Icone Questionnaire">
+        <img src="images_ced/icone1.png" alt="Icone Questionnaire">
         </a>
         <p>Questionnaire</p>
     </div>
     <div class="menu-item">
-    <a href="https://payspedia.alwaysdata.net/graph.php"><img src="/images/images_ced/icone2.png" alt="Icone Statistiques & Graphs" ></a>
+    <a href="https://payspedia.alwaysdata.net/graph.php"><img src="images_ced/icone2.png" alt="Icone Statistiques & Graphs" ></a>
         <p>Statistiques & Graphs</p>
     </div>
     <div class="menu-item">
-    <a href="https://payspedia.alwaysdata.net/forum/forum.php"><img src="/images/images_ced/icone7.png" alt="Forum"></a>
+    <a href="https://payspedia.alwaysdata.net/forum/forum.php"><img src="images_ced/icone3.png" alt="Forum"></a>
         <p>Forum</p>
     </div>
     <div class="menu-item logo">
-    <a href="https://payspedia.alwaysdata.net/"><img src="/images/images_ced/logo.png" alt="Logo"></a>
+    <a href="https://payspedia.alwaysdata.net/"><img src="images_ced/logo.png" alt="Logo"></a>
         
     </div>
     <div class="menu-item">
-    <a href="https://payspedia.alwaysdata.net/informations/informations.php"><img src="/images/images_ced/icone4.png" alt="Icone Informations"></a>
+    <a href="https://payspedia.alwaysdata.net/informations/informations.php"><img src="images_ced/icone4.png" alt="Icone Informations"></a>
         <p>Informations</p>
     </div>
     <div class="menu-item">
-    <a href="https://payspedia.alwaysdata.net/informations/sources.php"><img src="/images/images_ced/icone5.png" alt="Icone Sources données"></a>
+    <a href="https://payspedia.alwaysdata.net/informations/sources.php"><img src="images_ced/icone5.png" alt="Icone Sources données"></a>
         <p>Sources données</p>
     </div>
     <div class="menu-item">
-    <a href="https://payspedia.alwaysdata.net/utilisateur/profil.php"><img src="/images/images_ced/icone6.png" alt="Icone Options"></a>
+    <a href="https://payspedia.alwaysdata.net/utilisateur/profil.php"><img src="images_ced/icone6.png" alt="Icone Options"></a>
         <p>Profil</p>
     </div>
     </header>
