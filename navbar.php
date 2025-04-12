52cffb3676d43f6055602d0e1bd8b05b954ae5f4
Bogdan981011 <122132248+Bogdan981011@users.noreply.github.com>
2025-04-12 04:15:54 +0200

    adding navbar and pics

---
+++ navbar.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="menu-bar">
+    <div class="menu-item">
+    <a href="https://map-system-production.up.railway.app/questionnaire.php">
+        <img src="/images/images_ced/icone1.png" alt="Icone Questionnaire">
+        </a>
+        <p>Questionnaire</p>
+    </div>
+    <div class="menu-item">
+    <a href="https://payspedia.alwaysdata.net/graph.php"><img src="/images/images_ced/icone2.png" alt="Icone Statistiques & Graphs" ></a>
+        <p>Statistiques & Graphs</p>
+    </div>
+    <div class="menu-item">
+    <a href="https://payspedia.alwaysdata.net/forum/forum.php"><img src="/images/images_ced/icone7.png" alt="Forum"></a>
+        <p>Forum</p>
+    </div>
+    <div class="menu-item logo">
+    <a href="https://payspedia.alwaysdata.net/"><img src="/images/images_ced/logo.png" alt="Logo"></a>
+        
+    </div>
+    <div class="menu-item">
+    <a href="https://payspedia.alwaysdata.net/informations/informations.php"><img src="/images/images_ced/icone4.png" alt="Icone Informations"></a>
+        <p>Informations</p>
+    </div>
+    <div class="menu-item">
+    <a href="https://payspedia.alwaysdata.net/informations/sources.php"><img src="/images/images_ced/icone5.png" alt="Icone Sources données"></a>
+        <p>Sources données</p>
+    </div>
+    <div class="menu-item">
+    <a href="https://payspedia.alwaysdata.net/utilisateur/profil.php"><img src="/images/images_ced/icone6.png" alt="Icone Options"></a>
+        <p>Profil</p>
+    </div>
+    </header>
